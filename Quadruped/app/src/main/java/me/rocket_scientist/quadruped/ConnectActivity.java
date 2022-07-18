04bc13c01e607d703d9cfcc7cf9ba75c52587e43
Rocket-Scientist <info@rocket-scientist.me>
2022-07-18 17:06:51 +0300

    Encryption removed

--- Quadruped/app/src/main/java/me/rocket_scientist/quadruped/ConnectActivity.java
+++ Quadruped/app/src/main/java/me/rocket_scientist/quadruped/ConnectActivity.java
@@ -78,112 +78,6 @@ public class ConnectActivity extends FragmentActivity {
     Vibrator vibrator;
     public static ConnectActivity FConnectActivity;
 
-
-    //CRYPT
-    static char[] modulusTx = {
-            0xa5, 0x08, 0x16, 0x4b, 0x42, 0xb6, 0x73, 0x84, 0xc1, 0xeb, 0x9f, 0x93, 0xaa, 0x42,
-            0xd4, 0xff, 0xee, 0x75, 0xfa, 0x90, 0x7b, 0x62, 0xce, 0x39, 0xb9, 0x2e, 0x44, 0x9a, 0x54,
-            0xd1, 0x53, 0xb5, 0xe6, 0x3f, 0xf4, 0x58, 0xd1, 0x23, 0x23, 0xa7, 0x0f, 0x73, 0x81, 0xef,
-            0xd4, 0x90, 0xae, 0xae, 0x67, 0x77, 0xd1, 0xa5, 0x89, 0xc0, 0x46, 0x3b, 0xab, 0x26, 0x42,
-            0xf5, 0xb9, 0x3d, 0xb2, 0xc5, 0xe5, 0x66, 0x04, 0x7e, 0xa4, 0x5f, 0xec, 0xeb, 0xce, 0xda,
-            0x63, 0x08, 0x59, 0xf7, 0x6d, 0xf9, 0x8b, 0xab, 0xcf, 0x30, 0x1c, 0x24, 0x74, 0xb5, 0xc2,
-            0x6f, 0x7e, 0x3b, 0x8b, 0x55, 0x85, 0xcd, 0x74, 0xf6, 0xa3, 0x32, 0x7c, 0x3f, 0x80, 0xd0,
-            0xef, 0xda, 0x78, 0x37, 0x8d, 0x72, 0x87, 0xbc, 0xf0, 0x03, 0xf9, 0xae, 0xb6, 0xde, 0xae,
-            0xf8, 0x5e, 0xd5, 0xb9, 0xd8, 0xbd, 0x29, 0xf7, 0x8d
-    };
-    static char[] exponentPrivateTx = {
-            0x96, 0x50, 0x23, 0x5e, 0x31, 0x49, 0x55, 0x36, 0x5a, 0x1a, 0x6e, 0xa2, 0x64, 0xb5,
-            0xb2, 0xa3, 0xd9, 0x62, 0x83, 0xf2, 0x0d, 0x30, 0x15, 0x7d, 0xe5, 0xf9, 0x0a, 0x6b, 0x37,
-            0x4d, 0xe1, 0xdd, 0x0f, 0xf6, 0xb8, 0x6c, 0x1d, 0x30, 0xd0, 0x4c, 0x5d, 0x7a, 0x6b, 0xa3,
-            0x32, 0x0d, 0x05, 0xcd, 0xbc, 0x58, 0x1a, 0x0f, 0x98, 0xc4, 0x14, 0x34, 0x4c, 0xd2, 0xf0,
-            0xb2, 0x37, 0x22, 0xc7, 0x2c, 0x9a, 0x32, 0xbb, 0xd5, 0x99, 0x28, 0xe6, 0xa1, 0xe9, 0xbf,
-            0xc0, 0x2e, 0x5b, 0x99, 0x17, 0x72, 0x69, 0x92, 0x81, 0x25, 0x79, 0x1b, 0x29, 0x69, 0x2c,
-            0x24, 0x31, 0x6b, 0x2c, 0x35, 0xdc, 0xa0, 0x8e, 0xcf, 0x6c, 0xf3, 0x5a, 0xbb, 0x9a, 0xbf,
-            0x32, 0xec, 0x22, 0x9b, 0x38, 0x0e, 0xc3, 0xbb, 0x40, 0x96, 0xc5, 0x59, 0x4e, 0x1f, 0x36,
-            0x93, 0x7c, 0xb7, 0x50, 0xba, 0xb6, 0x00, 0xdf, 0x01
-    };
-    static char[] exponentPublicTx = {
-            0x01, 0x00, 0x01
-    };
-    static char[] modulusRx = {
-            0xaa, 0x33, 0x5f, 0x48, 0x88, 0xb0, 0x71, 0xcb, 0x4d, 0xc7, 0x1e, 0x28, 0x5c, 0x62,
-            0x74, 0x03, 0x76, 0xdf, 0x67, 0xff, 0x64, 0xce, 0x24, 0xe4, 0x5c, 0x89, 0xaa, 0x4d, 0xbb,
-            0x72, 0xa6, 0x7d, 0xfc, 0x4e, 0x49, 0x0e, 0xbc, 0x27, 0x45, 0x3b, 0x75, 0x0e, 0x9a, 0x6a,
-            0x62, 0xd5, 0x8a, 0x06, 0xa9, 0xf8, 0xff, 0x7d, 0x07, 0xa4, 0xba, 0xd6, 0x6b, 0x50, 0x09,
-            0x87, 0x40, 0xf7, 0xb9, 0x2d, 0xaf, 0xe3, 0x7b, 0x06, 0x8b, 0x09, 0x01, 0xf5, 0x00, 0xeb,
-            0x86, 0xd9, 0xdb, 0x2e, 0x2a, 0x4b, 0x70, 0x28, 0xed, 0x41, 0x90, 0x43, 0x75, 0x61, 0x5f,
-            0x86, 0x8b, 0xcb, 0x50, 0x8a, 0x0d, 0xf3, 0x67, 0xb1, 0xf6, 0x6e, 0x11, 0x46, 0x28, 0x67,
-            0xe3, 0x33, 0x6b, 0x1c, 0x8a, 0x55, 0xa7, 0x3c, 0x44, 0x21, 0x87, 0xce, 0x42, 0xe1, 0x78,
-            0xa2, 0x29, 0x23, 0x88, 0xac, 0x94, 0xde, 0x3e, 0x13
-    };
-    static char[] exponentPublicRx = {
-            0x01, 0x00, 0x01
-    };
-    PublicKey publicKeyTx;
-    PrivateKey privateKeyTx;
-    PublicKey publicKeyRx;
-    byte[] randomarrINT = new byte[86]; //ამაში ვწერ დაგენერირებულ შემთხვევით რიცხვს
-    String randomarrTXT; //ამაში ვწერ იგივე რიცხვს ტექსტად
-    String randomarrTxtEnc; //ამაში ვწერ შემთხვევით რიცხვებს ჯერ დაშიფრულს და მერე ტექსტად ქცეულს (არ ვიცი რად გვინდა)
-/*
-ვშიფრავ publicKeyRx
-ვაგზავნი
-გაშიფრავს თავისი პრივატე კლუჩით, დაშიფრავს publicKeyTx
-მიგზავნის
-გავშიფრავ privateKeyTx,
-შევადარებ თუ ისაა რაც გავაგზავნე
-*/
-
-    public static BigInteger IntArrToBigIntegerArr(char[] t){
-        ByteBuffer b = ByteBuffer.allocate(t.length);
-        int i = 0;
-        char tmp = 0;
-        while(i != t.length){
-            if(i+1 == t.length){
-                b.put((byte)t[i]);
-                i+=1;
-            }else {
-                b.putChar((char) ((t[i] << 8) | (t[i + 1])));
-                i+=2;
-            }
-        }
-        return new BigInteger(1, b.array());
-    }
-    private static PrivateKey getPrivateKeyFromBinary(char[] modulus, char[] privateExponent) {
-        try{
-            BigInteger privateExponentInt = IntArrToBigIntegerArr(privateExponent);
-            BigInteger keyInt = IntArrToBigIntegerArr(modulus);
-            RSAPrivateKeySpec privateKeySpec = new RSAPrivateKeySpec(keyInt, privateExponentInt);
-            KeyFactory factory = KeyFactory .getInstance("RSA");
-            return factory.generatePrivate(privateKeySpec);//KeyFactory.getInstance("RSA").generatePrivate(new RSAPrivateKeySpec(cm, ce));
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-    private static PublicKey getPublicKeyFromBinary(char[] modulus, char[] publicExponent) {
-        try{
-            BigInteger publicExponentInt = IntArrToBigIntegerArr(publicExponent);
-            BigInteger keyInt = IntArrToBigIntegerArr(modulus);
-            RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(keyInt, publicExponentInt);
-            KeyFactory factory = KeyFactory .getInstance("RSA");
-            return factory.generatePublic(publicKeySpec);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public byte[] RSAEncrypt(byte plain[], PublicKey key)throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        return cipher.doFinal(plain);
-    }
-    public byte[] RSADecrypt(final byte[] encryptedBytes, PrivateKey key)throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.DECRYPT_MODE, key);
-        return cipher.doFinal(encryptedBytes);
-    }
     public byte[] StringToByte(String s){
         int len = s.length();
         byte[] data = new byte[len / 2];
@@ -192,11 +86,6 @@ public class ConnectActivity extends FragmentActivity {
                     + Character.digit(s.charAt(i+1), 16));
         }
         return data;
-    }
-    public void RSAInit(){
-        publicKeyTx = getPublicKeyFromBinary(modulusTx,exponentPublicTx);
-        privateKeyTx = getPrivateKeyFromBinary(modulusTx,exponentPrivateTx);
-        publicKeyRx = getPublicKeyFromBinary(modulusRx,exponentPublicRx);
     }
     public static String bytesToHex(byte[] bytes) {
         final char[] hexArray = "0123456789ABCDEF".toCharArray();
@@ -208,27 +97,6 @@ public class ConnectActivity extends FragmentActivity {
         }
         return new String(hexChars);
     }
-    public void SendHandshake(){
-        RSAInit();
-        //Generate Random number
-        int i = 0;
-        while(i != 86){
-            randomarrINT[i] = (byte)(new Random().nextInt(255) + 1);
-            i++;
-        }
-        //Also fill as HEX text
-        randomarrTXT = bytesToHex(randomarrINT);
-        //Encrypt INT with publicKeyRx
-        try{
-            randomarrTxtEnc = bytesToHex(RSAEncrypt(randomarrINT, publicKeyRx));
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        //Send
-        //bluetoothSerial.write("@".concat(String.format("%04d", randomarrTxtEnc.length())).concat(randomarrTxtEnc));//4simbolo teqstis sigrdze
-        bluetoothSerial.write("@".concat(randomarrTxtEnc));
-    }
-    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -353,28 +221,13 @@ public class ConnectActivity extends FragmentActivity {
 
             @Override
             public void onBluetoothDeviceConnected(String name, String address) {
-                SendHandshake();
+                startActivity(JoystickIntent);
+                //STOP AND DESTROY TIMEOUT TIMER HERE
             }
 
             @Override
             public void onBluetoothSerialRead(String message) {
-                if(message.charAt(0) == '@'){
-                    //Decrypt and compare here
-                    if(message.length() >= 258){
-                        StringBuilder b = new StringBuilder(message);
-                        b.deleteCharAt(0);
-                        b.deleteCharAt(256);
-                        try{
-                            byte[] retval = RSADecrypt(StringToByte(b.toString()), privateKeyTx);
-                            if(Arrays.equals(retval, randomarrINT)){
-                                startActivity(JoystickIntent);
-                                //STOP AND DESTROY TIMEOUT TIMER HERE
-                            }
-                        }catch (Exception e){
-                            e.printStackTrace();
-                        }
-                    }
-                }else if (message.charAt(0) == '#') {
+                if (message.charAt(0) == '#') {
                     if (message.charAt(1) == 'C') {
                         if(SettingsActivity.RSettingsActivity){
                             SettingsActivity.calibresp = message;
